754bf7365d38c2bd267167fe7dbdac14521cd7f9
ahmedmaatty <am4201828@gmail.com>
2022-06-05 14:15:24 +0200

    now

---
+++ JS/main.js
@@ -0,0 +1,54 @@
+const list = document.querySelector(".nav-items");
+const mnu = document.getElementById("menu");
+
+mnu.addEventListener("click", () => {
+    showmenu ()
+});
+
+function showmenu () {
+    mnu.classList.toggle("is-active");
+    list.classList.toggle("is-active");
+};
+
+
+const first = document.getElementById("st")
+const second = document.getElementById("nd")
+const third = document.getElementById("rd")
+
+function closemenu() {
+    mnu.classList.toggle("is-active");
+    list.classList.toggle("is-active");
+}
+
+first.addEventListener("click",() => {
+    closemenu()
+})
+
+second.addEventListener("click",() => {
+    closemenu()
+})
+
+third.addEventListener("click",() => {
+    closemenu()
+})
+
+let div = document.getElementById("up")
+
+window.onscroll = function () {
+    if(this.scrollY >= 600){
+        div.classList.add("show")
+    }else{
+        div.classList.remove("show")
+    }
+}
+
+function  ToUp(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+}
+
+div.addEventListener("click",() => {
+    ToUp()
+})
